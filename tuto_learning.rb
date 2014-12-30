8da67a6e0c394909f71d6a19acc60d24ea064175
weyewe <w.yunnal@gmail.com>
2014-12-30 10:59:20 +0700

    new scrapper for download sites

--- tuto_learning.rb
+++ tuto_learning.rb
@@ -3,10 +3,12 @@ require 'nokogiri'
 require 'open-uri'
 require 'cgi'
 
-BASE_URL = 'http://avaxsearch.org' # http://avaxsearch.com/avaxhome_search?q=sex&commit=Go
+# http://learningdl.com/page/2/?s=udemy
+
+BASE_URL = 'http://tutolearning.com' # http://avaxsearch.com/avaxhome_search?q=sex&commit=Go
 PAGE_COUNT = 20
 
-FILENAME = 'avax_search.txt'
+FILENAME = 'tutolearning_search.txt'
 
 # http://avaxsearch.com/avaxhome_search?q=sex&commit=Go
 # "marakana"
@@ -15,7 +17,10 @@ search_keyword = CGI.escape( search_value )
 
 
 
-url = "#{BASE_URL}/avaxhome_search?q=#{search_keyword}&commit=Go"
+# http://learningdl.com/?s=udemy
+
+
+url = "#{BASE_URL}/search/#{search_keyword}"
 
 file_content = ""
 counter = 1
@@ -25,7 +30,7 @@ file_content << "Search Keyword: '#{search_value}'\n\n"
 (1..PAGE_COUNT).each do |page_count|
   page = Nokogiri::HTML(open(  url     ))  
 
-  results = page.css("div.item h3 a")
+  results = page.css("h2.entry-title a")
   results.each do |result|
     title = result.text
     link  = "#{result.attributes['href'].value}"
@@ -35,9 +40,9 @@ file_content << "Search Keyword: '#{search_value}'\n\n"
     counter += 1 
   end
   
-  next_page = page.css("a.next_page").first 
+  next_page = page.css("div#page-nav a.next").first 
   break if next_page.nil?
-  url = "#{BASE_URL}#{next_page.attributes['href'].value}" 
+  url = "#{next_page.attributes['href'].value}" 
 end
 
 File.open(FILENAME , 'w') {|f| f.write(file_content) }
