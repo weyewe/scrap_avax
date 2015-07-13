37163d63ac5997b6c709751d90a8337641128819
willy <w.yunnal@gmail.com>
2015-07-13 11:53:10 +0700

    modification for scrapper

--- scrap_search.rb
+++ scrap_search.rb
@@ -3,14 +3,14 @@ require 'nokogiri'
 require 'open-uri'
 require 'cgi'
 
-BASE_URL = 'http://avaxsearch.org' # http://avaxsearch.com/avaxhome_search?q=sex&commit=Go
+BASE_URL = 'http://avxsearch.se' # http://avaxsearch.com/avaxhome_search?q=sex&commit=Go
 PAGE_COUNT = 20
 
 FILENAME = 'avax_search.txt'
 
 # http://avaxsearch.com/avaxhome_search?q=sex&commit=Go
 # "marakana"
-search_value =  "agency"
+search_value =  "python"
 search_keyword = CGI.escape( search_value )
 
 
@@ -23,9 +23,10 @@ counter = 1
 file_content << "Search Keyword: '#{search_value}'\n\n"
 
 (1..PAGE_COUNT).each do |page_count|
+  puts "page: #{page_count}"
   page = Nokogiri::HTML(open(  url     ))  
 
-  results = page.css("div.item h3 a")
+  results = page.css("div.article a.title-link")
   results.each do |result|
     title = result.text
     link  = "#{result.attributes['href'].value}"
@@ -35,7 +36,7 @@ file_content << "Search Keyword: '#{search_value}'\n\n"
     counter += 1 
   end
   
-  next_page = page.css("a.next_page").first 
+  next_page = page.css("div.pagination a[rel=next]").first 
   break if next_page.nil?
   url = "#{BASE_URL}#{next_page.attributes['href'].value}" 
 end
